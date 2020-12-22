1c6934070c4cbbe150b12a13a06c22e311fb0a78
anhaidcca <anhaidcca@gmail.com>
2020-12-22 12:40:57 -0600

    22dic1240

---
+++ funciones.js
@@ -0,0 +1,14 @@
+fetch(‘https://randomuser.me/api/’);
+
+function mostrarPersona(datos){
+   const nombre= datos.results[0].name;
+   const foto= datos.results[0].picture.large;
+}
+
+const divPersona = document.querySelector("#persona");
+
+divPersona.innerHTML = ${nombre.first} ${nombre.last}
+
+ fetch('https://randomuser.me/api/')
+          .then(respuesta => respuesta.json())
+          .then(persona => mostrarPersona(persona));
